2a244d734a44b5391c095c3a767853e428346cb1
EricChien1126 <a0923190383@yahoo.com.tw>
2024-08-06 12:25:21 -0600

    week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>EricChien</p>
+      <Link href="https://github.com/EricChien1126/cprg306-assignments">
+        <a>My GitHub Repository</a>
+      </Link>
+    </div>
+  );
+}
