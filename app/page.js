c1d3ca8cc016698bfce1ae5e42734c2fb58125e5
EricChien1126 <a0923190383@yahoo.com.tw>
2024-08-06 15:07:19 -0600

    Add Week 4 Assignment

--- app/page.js
+++ app/page.js
@@ -132,3 +132,20 @@ export default function HomePage() {
     </main>
   );
 }
+// /app/page.js
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Assignments</h1>
+      <ul>
+        <li><Link href="/week-1">Week 1</Link></li>
+        <li><Link href="/week-2">Week 2</Link></li>
+        <li><Link href="/week-3">Week 3</Link></li>
+        <li><Link href="/week-4">Week 4</Link></li> {/* Add this line */}
+      </ul>
+    </div>
+  );
+}
+
